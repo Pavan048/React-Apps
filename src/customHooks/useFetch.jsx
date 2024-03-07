183f479b3cae1cb8f7966626c86758887bde841a
Pavan048 <pavankrishna048@gmail.com>
2024-03-08 02:02:17 +0530

    adding custom hooks

--- src/customHooks/useFetch.jsx
+++ src/customHooks/useFetch.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import { useState , useEffect } from "react"
 
-const useFetch = () => {
-  return (
-    <div>
-      useFetch
-    </div>
-  )
+const useFetch = (url) => {
+
+const [data , setData] = useState("");
+const [loading , setLoading] = useState(false);
+const [isError , setIsError]  = useState({status:false , msg:""});
+
+const makeApiCall = async (url) => {
+  setLoading(true);
+  setIsError({status:false , msg:""});
+  try {
+    const comingData = await fetch(url);
+    const response = await comingData.json();
+    setData(response);
+    setLoading(false);
+    
+} catch (error) {
+    setIsError({status:true , msg: error.message || "Something went wrong"});
+    setLoading(false);
+  }
+
+}
+
+useEffect(() => {
+  makeApiCall(url);
+  setIsError({status:false , msg:""});
+
+},[])
+
+return [data , loading , isError]
+
+ 
 }
 
 export default useFetch
